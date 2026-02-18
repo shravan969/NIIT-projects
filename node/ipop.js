28a2e66486febf8b24f008de1ebe0c3404779d66
Shravan <shravanbichkule969gmail.com>
2026-02-18 15:55:12 +0530

    update

---
+++ node/ipop.js
@@ -0,0 +1,16 @@
+const process = require('node:process');
+
+let name =  process.argv[2];
+console.log("hello ",name);
+
+
+
+________________________
+
+const prompt = require("prompt-sync")
+
+let name = prompt("Enter your name: ")
+
+console.log("Hello",name);
+
+___________________________________________________
